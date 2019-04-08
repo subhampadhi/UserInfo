74bb852d72e637e67538623320a9af57baaa9b9d
Subham Padhi <subhampadhi15@gmail.com>
2019-04-08 23:13:39 +0530

    Initial Commit

---
+++ UserInfo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  UserInfo
+//
+//  Created by Subham Padhi on 08/04/19.
+//  Copyright © 2019 Subham Padhi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
